a357db08246596f8c558eb361c15bd18c0ceb87a
notGonim <mhmodgonim96@gmail.com>
2021-04-11 00:18:51 +0200

    CREATED : useBookSearch component where i handle all the logic for getting books data from the API

---
+++ src/components/UseBookSearch.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react'
+
+
+export const UseBookSearch = ({ query, pageNum }) => {
+
+
+    useEffect(() => {
+
+    }, query, pageNum)
+    return (
+        <div>
+
+        </div>
+    )
+}
